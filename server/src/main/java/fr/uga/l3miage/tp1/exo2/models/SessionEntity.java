36cb272da552989327c1aaf437c943aa48d8b663
DIALLO Mamadou Hassimiou <mamadou.diallo4@etu.univ-grenoble-alpes.fr>
2024-03-17 23:42:00 +0100

    ajout des getteurs et setteurs

--- server/src/main/java/fr/uga/l3miage/tp1/exo2/models/SessionEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo2/models/SessionEntity.java
@@ -1,11 +1,17 @@
 package fr.uga.l3miage.tp1.exo2.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import java.util.UUID;
 
 @Entity
+@Accessors
+@Getter
 public class SessionEntity {
     @Id
     private UUID idSession;
--- server/src/main/java/fr/uga/l3miage/tp1/exo2/models/UserEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo2/models/UserEntity.java
@@ -1,11 +1,16 @@
 package fr.uga.l3miage.tp1.exo2.models;
 
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import java.time.OffsetDateTime;
 
 @Entity
+@Accessors
+@Getter
 public class UserEntity {
     @Id
     private Long idUser;
